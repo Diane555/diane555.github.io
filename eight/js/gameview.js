f0a63ba8774cbf736570633b81fcbbe71d01bcfd
diane555 <dianesarbeitskonto@gmail.com>
2021-11-24 12:25:02 +0000

    helpme

--- eight/js/gameview.js
+++ eight/js/gameview.js
@@ -3,7 +3,7 @@ const ctx = canvas.getContext('2d')
 const d = 3
 const tileWidth = canvas.width / d
 const tileHeight = canvas.height / d
-const tileColor = '#000000'
+const tileColor = '#ffffff'
 const canvasColor = '#eee'
 canvas.style.backgroundColor = canvasColor
 
